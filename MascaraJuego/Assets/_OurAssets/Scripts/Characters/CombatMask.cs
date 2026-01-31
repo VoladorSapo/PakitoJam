0d92ead14934fe6af4634b384f0fb2b9361a6f3f
VoladorSapo <voladorsapo@gmail.com>
2026-01-31 21:42:32 +0100

    separar ACharacter

---
+++ MascaraJuego/Assets/_OurAssets/Scripts/Characters/CombatMask.cs
@@ -0,0 +1,20 @@
+public class CombatMask : APowerMask
+{
+   
+
+    public override void Die()
+    {
+        _character.Die();
+    }
+
+    public override void receiveDamage(int damage)
+    {
+        _character.getDamaged(damage);
+    }
+
+    public override MaskTypes type()
+    {
+        return MaskTypes.CombatMask;
+    }
+ 
+}
